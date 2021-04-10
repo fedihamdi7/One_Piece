765bc98e5bfe2611706b90e8a7bc4a060f595de2
Fedi Hamdi <fedihamdi97@gmail.com>
2021-04-10 10:51:16 +0100

    n

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','HomeController@welcome');
