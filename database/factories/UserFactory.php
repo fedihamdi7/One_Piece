f67c6065de6a4e819b5b593ece17283e098abc83
eyabenaouicha <eyabenaouicha25@gmail.com>
2021-04-27 00:57:17 +0100

    Adding factory+seed to User table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,10 +20,11 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker-> freeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'image' => $faker->image(),
-        'type' => $faker->randomElement(['admin', 'responsable','membre'])
+        'password'=> $faker -> password,
+        'image' => $faker-> imageUrl(),
+        'type' => $faker-> randomElement(['admin', 'responsable','membre']),
+        'remember_token' => $faker -> word,
     ];
 });
