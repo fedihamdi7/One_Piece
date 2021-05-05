c74956faa6a4c694f7e8fbc200ed739e8ab76b61
Fedi Hamdi <fedihamdi97@gmail.com>
2021-05-06 00:40:23 +0100

    add Club and Clubs template

---
+++ resources/views/clubs.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="wrapper">
+    <div class="container-fostrap">
+        <div>
+            {{-- <img src="https://4.bp.blogspot.com/-7OHSFmygfYQ/VtLSb1xe8kI/AAAAAAAABjI/FxaRp5xW2JQ/s320/logo.png" class="fostrap-logo"/> --}}
+            <h1 class="heading">
+                All Clubs
+            </h1>
+        </div>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    @foreach ($clubs as $club)
+
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="/club/{{$club->club_id}}">
+                            <img src="images/{{$club -> club_img}}" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="/club/{{$club->club_id}}"> {{$club -> club_name}}
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    {{$club -> about_us}}
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <a href="/club/{{$club->club_id}}" class="btn btn-link btn-block">
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,15 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/club.js','public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.styles('resources/css/clubs.css','public/css/clubs.css');
+mix.styles([
+    'resources/css/fontawesome.css',
+    'resources/css/templatemo-style.css',
+    'resources/css/lightbox.css',
+    'resources/css/card_rotate.css',
+    'resources/css/one_club.css',
+    'resources/css/owl.carousel.min.css',
+    'resources/css/owl.theme.default.min.css',
+], 'public/css/club.css');
