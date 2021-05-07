35ee89ddee48fbb0520eebcbd4bf78bfe8656330
BalssemZoghbi <zoghbibalssem@gmail.com>
2021-05-07 01:54:26 +0100

    update

--- app/Http/Controllers/ResponsableController.php
+++ app/Http/Controllers/ResponsableController.php
@@ -8,7 +8,7 @@ class ResponsableController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
    public function responsable(){
      return view('responsable.dashboard');
