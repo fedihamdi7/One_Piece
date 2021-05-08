c97a88cf4db2ea2596db912bdd82fd7abe3254d6
eyabenaouicha <eyabenaouicha25@gmail.com>
2021-05-08 19:30:21 +0100

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,12 +19,12 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker-> freeEmail,
+        'user_name' => $faker->name,
+        'user_email' => $faker-> freeEmail,
         'email_verified_at' => now(),
-        'password'=> $faker -> password,
-        'image' => $faker-> imageUrl(),
-        'type' => $faker-> randomElement(['admin', 'responsable','membre']),
+        'user_password'=> $faker -> password,
+        'user_image' => $faker-> imageUrl(),
+        'user_type' => $faker-> randomElement(['admin', 'responsable','membre']),
         'remember_token' => $faker -> word,
     ];
 });
