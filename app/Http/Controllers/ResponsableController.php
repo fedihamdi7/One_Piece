b4a8ccd0f6f572d7e6e078010f345277b7345108
Fedi Hamdi <fedihamdi97@gmail.com>
2021-05-13 21:51:48 +0100

    implementing Middlewares

--- app/Http/Controllers/ResponsableController.php
+++ app/Http/Controllers/ResponsableController.php
@@ -9,6 +9,7 @@ class ResponsableController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('responsable');
     }
    public function responsable(){
      return view('responsable.dashboard');
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'responsable' => \App\Http\Middleware\CheckIfResponsable::class,
+        'admin' => \App\Http\Middleware\CheckIfAdmin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
